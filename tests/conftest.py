0cbcfc8add387b036a65182d63743db043bba5b1
Aviel Galkin <aviel@aporia.com>
2022-06-27 11:48:18 +0300

    docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""Package-wide test fixtures."""
+
 from unittest.mock import Mock
 
 from _pytest.config import Config
@@ -6,12 +8,14 @@ from pytest_mock import MockFixture
 
 
 def pytest_configure(config: Config) -> None:
+    """Pytest configuration hook."""
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
 
 
 @pytest.fixture
 def mock_requests_get(mocker: MockFixture) -> Mock:
-    mock = mocker.patch("requests.get")
+    """Fixture for mocking requests.get."""
+    mock: Mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Lorem Ipsum",
         "extract": "Lorem ipsum dolor sit amet",
